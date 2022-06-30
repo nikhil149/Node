eb7e3e811cd6ad986b9941499fea35d19c7b9566
nikhil149 <nikhil123rachawar@gmail.com>
2022-06-30 11:49:02 +0530

    Added middlewares

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next()
+});
+
+app.use((req, res, next) => {
+  console.log("In the another middleware");
+});
 const server = http.createServer(app);
 server.listen(3000);
