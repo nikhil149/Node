d4ca5d910a0a81b0b4d93d3c883997516ee33c01
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-02 18:11:29 +0530

    Paths are being filters

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // routes
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
