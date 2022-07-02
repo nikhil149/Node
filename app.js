123d1891303d72b366b833ed03c7f629fe29fc15
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-02 17:25:18 +0530

    Added path

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
   console.log("In the middleware");
-  next()
+  res.send('<h1>Add Product</h1>')
 });
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
   console.log("In the another middleware");
+  res.send('<h1>Welcome to home page</h1>')
 });
 
 app.listen(3000)
