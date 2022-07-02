2472e22b81a5a226985df1777c90b426cba8bc86
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-02 17:52:11 +0530

    aded post instead of use

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(JSON.parse(JSON.stringify((req.body))));
   res.redirect("/");
 });
