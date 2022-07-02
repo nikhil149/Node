4354c00b723a56afe76cc0af22d47907ecabf292
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-02 17:48:35 +0530

    body parser installed and added

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('/add-product',(req, res, next) => {
-  console.log("In the middleware");
-  res.send('<h1>Add Product</h1>')
+app.use(bodyParser.urlencoded({ extended: false }));
+// routes
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+  );
 });
 
-app.use('/',(req, res, next) => {
-  console.log("In the another middleware");
-  res.send('<h1>Welcome to home page</h1>')
+app.use("/product", (req, res, next) => {
+  console.log(JSON.parse(JSON.stringify((req.body))));
+  res.redirect("/");
 });
 
-app.listen(3000)
+app.use("/", (req, res, next) => {
+  res.send("<h1>Welcome to home page</h1>");
+});
+
+app.listen(3000);
