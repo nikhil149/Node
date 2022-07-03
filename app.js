04381e069ade8a3935e4d0c97b75f4662e26ca69
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-03 17:32:43 +0530

    shop pug file created and rendered

--- app.js
+++ app.js
@@ -8,14 +8,17 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // routes
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
