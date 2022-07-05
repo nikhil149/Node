1f02401bc7d94ff2b97215e9878aca449c6ff307
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-05 09:42:30 +0530

    404 ejs created

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", {pageTitle: "Page Not Found"});
 });
 
 app.listen(3000);
