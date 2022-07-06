e5c741230df642aedbec3420c9cecce1f0a64627
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-06 11:24:13 +0530

    added multiple views in views folder

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require("../models/product");
 exports.getAddProducts = (req, res, next) => {
   //   res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("add-product", {
+  res.render("admin/add-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
   });
@@ -15,7 +15,7 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((product) => {
-    res.render("shop", {
+    res.render("shop/product-list", {
       prods: product,
       pageTitle: "Shop",
       path: "/",
