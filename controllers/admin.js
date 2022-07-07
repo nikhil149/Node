2655e282af2b3622d541b7da8306f5b00fa7289b
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-07 11:45:21 +0530

    edit product router and cotroller added with view

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,11 @@
+const { query } = require("express");
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true,
+    edit: false,
   });
 };
 
@@ -18,6 +17,25 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const productId = req.params.productId;
+  Product.findProductById(productId, (product) => {
+    if (!product) {
+      res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      edit: editMode === "true",
+      product: product,
+    });
+  });
 };
 
 exports.getProducts = (req, res, next) => {
