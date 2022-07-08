a4bd8f4cff93e7b6642cf58cf013e89f88fa29cb
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-08 11:22:47 +0530

    delete product from cart and admin products added

--- models/cart.js
+++ models/cart.js
@@ -30,4 +30,23 @@ module.exports = class Cart {
       });
     });
   }
+
+  static deleteProduct(id, price) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const cart = JSON.parse(fileContent);
+      const updatedCart = { ...cart };
+      const product = updatedCart.products.find((prod) => prod.id === id);
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        (prod) => prod.id !== id
+      );
+      updatedCart.totalPrice -= productQty * price;
+      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+        console.log(err);
+      });
+    });
+  }
 };
