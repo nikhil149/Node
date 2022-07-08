d21f851ddd80a71ff12ae4e038d8d173be6eb045
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-08 12:09:23 +0530

    Desplaying cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,17 +33,30 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    path: "/cart",
-    pageTitle: "Your Cart",
+  Cart.getCartProducts((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = products.map((product) => {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          return { productData: product, qty: cartProductData.qty };
+        } 
+      });
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: cartProducts, 
+      });
+    });
   });
 };
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  Product.findProductById(productId, product=>{
-    Cart.addProduct(product.id, product.price)
-  })
+  Product.findProductById(productId, (product) => {
+    Cart.addProduct(product.id, product.price);
+  });
   console.log(productId);
   res.redirect("/cart");
 };
