de5cdbdb245df202c52fd9bd63f7d841a087a9e2
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-12 11:51:16 +0530

    User model created

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 
 const sequelize = require("./util/database");
 
+const Product = require("./models/product");
+const User = require("./models/user");
+
 const errorController = require("./controllers/error");
 
 const adminRoutes = require("./routes/admin");
@@ -17,19 +20,38 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 // routes
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 sequelize
+  //   .sync({force: true})
   .sync()
   .then((result) => {
     // console.log(result);
-    app.listen(3000);
+    return User.findByPk(1);
   })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Nikhil", email: "test@test.com" });
+    }
+    return user;
+  })
+  .then(() => app.listen(3000))
   .catch((err) => {
     console.log(err);
   });
-
