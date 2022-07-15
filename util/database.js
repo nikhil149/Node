4ad37efd5d394544634e263a753bc7d9e384b626
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-15 12:06:16 +0530

    mongodb connection added

--- util/database.js
+++ util/database.js
@@ -1,8 +1,18 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("shop", "root", "123456789", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://nikhil:Nikhil149@shop.nrvc5.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("connected");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+// Nikhil149
 
-module.exports = sequelize;
+module.exports = mongoConnect;
