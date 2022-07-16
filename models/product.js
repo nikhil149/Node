2ccf0d513a1719c4de4e79105b8805b5951cf27a
nikhil149 <nikhil123rachawar@gmail.com>
2022-07-16 18:47:29 +0530

    Edit product using mongdb

--- models/product.js
+++ models/product.js
@@ -2,39 +2,29 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    // let dbOp;
+    if (this._id) {
+      return db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      return db.collection("products").insertOne(this);
+    }
   }
 
   static fetchAll() {
     const db = getDb();
-    return db
-      .collection("products")
-      .find()
-      .toArray()
-      .then((products) => {
-        console.log(products);
-        return products;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    return db.collection("products").find().toArray();
   }
 
   static findById(prodId) {
@@ -42,9 +32,7 @@ class Product {
     return db
       .collection("products")
       .find({ _id: new mongodb.ObjectId(prodId) })
-      .next()
-      .then()
-      .catch((err) => console.log(err));
+      .next();
   }
 }
 
